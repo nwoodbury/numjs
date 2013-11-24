13cb0741a203bb527ce7333c4eea21045e745a9a
Nathan Woodbury <nathanswoodbury@gmail.com>
2013-11-23 23:23:40 -0700

    Renamed array to narray.

--- test/core/array.js
+++ test/core/array.js
@@ -1,82 +1,78 @@
 'use strict';
 
-var array = require('../../lib/core/array.js');
+var narray = require('../../lib/core/narray.js');
 var assert = require('assert');
 //var should = require('should');
 
-describe('array', function() {
+describe('narray', function() {
 
     //=========================================================================
-    //  array.zeros
+    //  narray Creation
     //=========================================================================
 
     describe('#zeros(n)', function() {
 
         // Proper execution
         it('should create an array consisting of n 0s', function() {
-            assert.deepEqual([0, 0, 0, 0, 0], array.zeros(5));
-            assert.deepEqual([], array.zeros(0));
+            assert.deepEqual([0, 0, 0, 0, 0], narray.zeros(5));
+            assert.deepEqual([], narray.zeros(0));
         });
 
         // Input error checking
         it('should throw an error due to bad input', function() {
-            assert.throws(function() {array.zeros(-1);}, TypeError,
+            assert.throws(function() {narray.zeros(-1);}, TypeError,
                           'Input n must not be negative.');
-            assert.throws(function() {array.zeros('a');}, TypeError,
+            assert.throws(function() {narray.zeros('a');}, TypeError,
                           'Input n must be an integer.');
-            assert.throws(function() {array.zeros(2.4);}, TypeError,
+            assert.throws(function() {narray.zeros(2.4);}, TypeError,
                           'Input n must be an integer.');
         });
     });
-
-    //=========================================================================
-    //  array.ones
-    //=========================================================================
 
     describe('#ones(n)', function() {
 
         // Proper execution
         it('should create an array consisting of n 1s', function() {
-            assert.deepEqual([1, 1, 1, 1], array.ones(4));
-            assert.deepEqual([], array.ones(0));
+            assert.deepEqual([1, 1, 1, 1], narray.ones(4));
+            assert.deepEqual([], narray.ones(0));
         });
 
         // Input error checking
         it('should throw an error due to bad input', function() {
-            assert.throws(function() {array.ones(-1);}, TypeError,
+            assert.throws(function() {narray.ones(-1);}, TypeError,
                           'Input n must not be negative.');
-            assert.throws(function() {array.ones('a');}, TypeError,
+            assert.throws(function() {narray.ones('a');}, TypeError,
                           'Input n must be an integer.');
-            assert.throws(function() {array.ones(2.4);}, TypeError,
+            assert.throws(function() {narray.ones(2.4);}, TypeError,
                           'Input n must be an integer.');
         });
     });
-
-    //=========================================================================
-    //  array.sizefill
-    //=========================================================================
 
     describe('#sizefill(n, value)', function() {
 
         // Proper execution
         it('should create an array consisting of n copies of value',
            function() {
-            assert.deepEqual([1, 1, 1], array.sizefill(3, 1));
-            assert.deepEqual([], array.ones(0, 921));
-            assert.deepEqual([2.2, 2.2, 2.2, 2.2], array.sizefill(4, 2.2));
+            assert.deepEqual([1, 1, 1], narray.sizefill(3, 1));
+            assert.deepEqual([], narray.ones(0, 921));
+            assert.deepEqual([2.2, 2.2, 2.2, 2.2], narray.sizefill(4, 2.2));
         });
 
         // Input error checking
         it('should throw an error due to bad input', function() {
-            assert.throws(function() {array.sizefill(-1, 3);}, TypeError,
+            assert.throws(function() {narray.sizefill(-1, 3);}, TypeError,
                           'Input n must not be negative.');
-            assert.throws(function() {array.sizefill('a', 4);}, TypeError,
+            assert.throws(function() {narray.sizefill('a', 4);}, TypeError,
                           'Input n must be an integer.');
-            assert.throws(function() {array.sizefill(2.4, 78);}, TypeError,
+            assert.throws(function() {narray.sizefill(2.4, 78);}, TypeError,
                           'Input n must be an integer.');
 
-            assert.throws(function() {array.sizefill(3, 'a');}, TypeError,
+            assert.throws(function() {narray.sizefill(3, 'a');}, TypeError,
                           'Input value must be a number.');
         });
     });
+
+    //=========================================================================
+    //  narray type checking
+    //=========================================================================
 });
